eddbf21990b05ed8a4e386d72feed6a740f83c2d
rohit-siva-sai <106007387+rohit-siva-sai@users.noreply.github.com>
2023-06-05 20:51:39 +0530

    initial commit

--- components/training/offerings.js
+++ components/training/offerings.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Offering1 from "./matter/offering1";
 import Offering2 from "./matter/offering2";
 import Offering3 from "./matter/offering3";
@@ -7,43 +7,131 @@ import Offering4 from "./matter/offering4";
 import Offering5 from "./matter/offering5";
 
 const Offerings = () => {
+  const [active, setActive] = useState("");
+
+  const handleClick = (menuId) => {
+    setActive(menuId)
+  }
+
   return (
     <div className="md:px-48 px-4 py-12">
-     <p className="text-gray-800 text-3xl font-semibold ">Our AI service offerings</p>
+      <p className="text-gray-800 text-3xl font-semibold ">
+        Our AI service offerings
+      </p>
       <div className="pt-20 flex md:px-4 justify-between ">
         <div className="w-1/3  ">
-          <div className="sticky top-8 py-4   left-36 h-fit hidden md:flex flex-col space-y-5">
-            <Link href={"#1"}>
-              <p className="font-semibold hover:text-gray-600 ">AI Implementation Strategy</p>
+          <div className="sticky top-8 py-4   left-36 h-fit hidden md:flex flex-col  border-blue-500">
+            <Link
+              href={"#1"}
+              onClick={() => {
+                handleClick("1");
+              }}
+            >
+              <p
+                className={`font-semibold border-l-4 px-4 py-4 ${
+                  active === "1" ? "text-gray-500 border-blue-500" : ""
+                }  `}
+              >
+                AI Implementation Strategy
+              </p>
             </Link>
-            <Link href={"#2"}>
-              <p className="font-semibold hover:text-gray-600 ">Data Engineering</p>
+            <Link
+              href={"#2"}
+              onClick={() => {
+                handleClick("2");
+              }}
+            >
+              <p
+                className={`font-semibold  border-l-4 px-4 py-4 ${
+                  active === "2" ? "text-gray-500 border-blue-500" : ""
+                }  `}
+              >
+                Data Engineering
+              </p>
             </Link>
-            <Link href={"#3"}>
-              <p className="font-semibold hover:text-gray-600 ">AI Transformation Accelerator</p>
+            <Link
+              href={"#3"}
+              onClick={() => {
+                handleClick("3");
+              }}
+            >
+              <p
+                className={`font-semibold  border-l-4 px-4 py-4 ${
+                  active === "3" ? "text-gray-500 border-blue-500" : ""
+                }  `}
+              >
+                AI Transformation Accelerator
+              </p>
             </Link>
-            <Link href={"#4"}>
-              <p className="font-semibold hover:text-gray-600 ">ML Operations</p>
+            <Link
+              href={"#4"}
+              onClick={() => {
+                handleClick("4");
+              }}
+            >
+              <p
+                className={`font-semibold  border-l-4  px-4 py-4 ${
+                  active === "4" ? "text-gray-500 border-blue-500" : ""
+                }  `}
+              >
+                ML Operations
+              </p>
             </Link>
-            <Link href={"#5"}>
-              <p className="font-semibold hover:text-gray-600 ">ML Model Development</p>
+            <Link
+              href={"#5"}
+              onClick={() => {
+                handleClick("5");
+              }}
+            >
+              <p
+                className={`font-semibold  border-l-4 px-4 py-4 ${
+                  active === "5" ? "text-gray-500 border-blue-500" : ""
+                }  `}
+              >
+                ML Model Development
+              </p>
             </Link>
           </div>
         </div>
         <div className="flex md:w-2/3 flex-col space-y-12">
-          <div id="1">
+          <div
+            id="1"
+            onMouseOver={() => {
+              handleClick("1");
+            }}
+          >
             <Offering1 />
           </div>
-          <div id="2">
+          <div
+            id="2"
+            onMouseOver={() => {
+              handleClick("2");
+            }}
+          >
             <Offering2 />
           </div>
-          <div id="3">
+          <div
+            id="3"
+            onMouseOver={() => {
+              handleClick("3");
+            }}
+          >
             <Offering3 />
           </div>
-          <div id="4">
+          <div
+            id="4"
+            onMouseOver={() => {
+              handleClick("4");
+            }}
+          >
             <Offering4 />
           </div>
-          <div id="5">
+          <div
+            id="5"
+            onMouseOver={() => {
+              handleClick("5");
+            }}
+          >
             <Offering5 />
           </div>
         </div>
