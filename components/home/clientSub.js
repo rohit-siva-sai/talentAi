010a4de2f7aa8f5a6dd117bc3f8496cdb584bfa0
rohit-siva-sai <106007387+rohit-siva-sai@users.noreply.github.com>
2023-06-12 17:08:59 +0530

    initial commit

--- components/home/clientSub.js
+++ components/home/clientSub.js
@@ -8,7 +8,7 @@ const ClientSub = ({ text, post, imgUrl,name }) => {
           <img src="/bg.png" alt="" className="w-10" />
         </picture>
       </div>
-      <p className="px-6 text-gray-600 pt-4 pb-8 leading-6">"{text}"</p>
+      <p className="px-6 text-gray-600 pt-4 pb-8 leading-6">{text}</p>
       <div className="flex flex-col px-6 space-y-1 h-fit mt-auto pb-10 items-center">
         <span class="inline-block h-1 w-10 rounded bg-blue-500 mb-3"></span>
         <h2 class="text-gray-900 font-medium uppercase title-font tracking-wider text-sm">
